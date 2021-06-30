ede43d4b24a27c2ce46027e98a85224dc7c867cf
ampmasai <02.anush@gmail.com>
2021-06-30 21:03:29 +0530

    Exploring app selector

--- app/src/main/java/com/example/anushmp/customactivityappselector/MainActivity.java
+++ app/src/main/java/com/example/anushmp/customactivityappselector/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.anushmp.customactivityappselector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etIntent;
+    private Button intentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initviews();
+
+        intentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent sendInfoToApp2 = new Intent("com.fetchusername");
+                //sendInfoToApp2.setAction("fetchusername");
+                sendInfoToApp2.putExtra("key", etIntent.getText());
+                startActivity(sendInfoToApp2);
+
+            }
+        });
+
+
     }
+
+    private void initviews(){
+
+        etIntent = findViewById(R.id.etUsername);
+        intentButton = findViewById(R.id.intentbutton);
+
+    }
+
+
+
+
 }
